368a7e8a2e66f50e4e2f9eb55314f4b446426643
Noah <cooper.noah@gmail.com>
2018-11-09 03:47:04 -0800

    rewriting in python 3, fancier too

--- install.sh
+++
@@ -1,5 +0,0 @@
-mkdir /opt/gtk_sniffer/
-cp sniffer.py /opt/gtk_sniffer/
-cp sniff.sh /opt/gtk_sniffer/
-ln -s /opt/gtk_sniffer/sniffer.py /usr/sbin/sniff
-apt install gksu
--- uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf /opt/gtk_sniffer/
-rm /usr/sbin/sniff
