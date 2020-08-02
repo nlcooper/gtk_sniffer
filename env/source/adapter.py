531dfbe26df5002d183f00bb06cac695b42e50af
noah cooper <cooper.noah@gmail.com>
2020-08-02 14:31:46 -0700

    added sniffer class, methods

--- env/source/adapter.py
+++ env/source/adapter.py
@@ -4,23 +4,61 @@ import subprocess
 class Adapter():
     def __init__(self, name):
         self.name=name
+        self.wiphy=None
         self.mode=None
-        self.status=False
-        self.sniff=False
         self.MAC=None
+        self.get_info()
 
     @staticmethod
     def get_adapters():
         cmd = 'iw dev'
         proc = subprocess.run(cmd.split(), capture_output=True, encoding='utf-8')
-        return re.findall('Interface\s(\w*)', proc.stdout)
-        
+        return re.findall(r'Interface\s(\w*)', proc.stdout)
+
+    @staticmethod
+    def is_supported():
+        cmd = 'iw list'
+        proc = subprocess.run(cmd.split(), capture_output=True, encoding='utf-8')
+        if not 'monitor' in proc.stdout:
+            return False
+        else:
+            return True
+
+    def get_info(self):
+        cmd = f'iw {self.name} info'
+        proc = subprocess.run(cmd.split(), capture_output=True, encoding='utf-8')
+        self.wiphy = re.search(r'wiphy\s(\w)', proc.stdout)[1]
+        self.MAC = re.search(r'addr\s([0-9a-f]{2}(?::[0-9a-f]{2}){5})', proc.stdout)[1]
+        self.mode = re.search(r'type\s(\w*)', proc.stdout)[1]
+
+    def del_adapter(self):
+        cmd = f'iw dev {self.name} interface del'
+        proc = subprocess.run(cmd.split())
+
+    def add_adapter(self):
+        cmd = f'iw phy phy{self.wiphy} interface add type managed'
+        proc = subprocess.run(cmd.split())
+
 
 class Sniffer(Adapter):
     def __init__(self):
+        self.channel = None
+        self.width = None
         super.__init__(self)
 
+    def add_adapter(self):
+        cmd = f'iw dev phy phy{self.wiphy} interface add type monitor'
+        proc = subprocess.run(cmd.split())
 
+    def set_chan(self, channel, width):
+        cmd = f'iw {self.name} set channel {channel} {width}'
+        proc = subprocess.run(cmd.split())
+
+    def start_sniff(self):
+        cmd = f'tcpdump -ttttvvv -s0 -i {self.name} -w output.pcap'
+        proc = subprocess.run(cmd.split())
+        return proc
+        
 if __name__ == '__main__':
-  print('run')
-  a = Adapter.get_adapters()
+    print('run')
+    a = Adapter.get_adapters()
--- env/source/main.py
+++ env/source/main.py
@@ -1,21 +1,34 @@
 import adapter
 
+def test_support():
+    test = adapter.Adapter.is_supported()
+    if not test:
+        print('Unable to find wireless device capable of monitor mode.')
+        raise SystemExit(1)
+
+def select_adapter(adapters):
+    if len(adapters) == 1:
+        return adapters[0]
+    print('Please choose an adapter to use:')
+    for i, n in enumerate(adapters, start=1):
+       print(i, n)
+    choice = input('Interface number: ')
+    try:
+        choice = int(choice) - 1
+    except:
+        raise TypeError('Please input the number corresponding to your adapter.')
+    return adapters[choice]
+
 def main():
-    adapters = adapter.Adapter.get_adapters()
-    wifi = adapters[0]
-    if not len(adapters) == 1:
-        print('Please choose an adapter to use:')
-        for i, n in enumerate(adapters, start=1):
-           print(i, n)
-        choice = input('Interface number: ')
-        try:
-            choice = int(choice) - 1
-        except:
-            raise TypeError('Please input the number corresponding to your adapter.')
-        wifi = adapters[choice]
+    test_support()
+    tryit = adapter.Adapter.is_supported()
+    ifaces = adapter.Adapter.get_adapters()
+    wifi = select_adapter(ifaces)
     interface = adapter.Adapter(wifi)
     print(interface.name)
-
+    print(interface.wiphy)
+    print(interface.MAC)
+    print(interface.mode)
 if __name__ == '__main__':
     print('run')
     main()
